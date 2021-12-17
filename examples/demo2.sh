8723a2efc2ac9b891226e276a984fce485f5b16b
Stewart Reive <3587451+st3w@users.noreply.github.com>
2021-12-17 01:28:15 -0800

    Make "pct" options accept values in the range 0 to 100 inclusive
    Before, the --glitchpct, --rippct, and --shortpct options accepted
    values in the range [0,1]. Now they will take values in the range
    [0,100]. This is probably more consistent with how most users
    expect these options to work. However, it breaks backwards
    compatibility...

--- examples/demo2.sh
+++ examples/demo2.sh
@@ -2,4 +2,4 @@
 
 # This demo showcases the alternate shading mode
 
-neo --color=green3 --charset=hex --shortpct=0 --shadingmode=1 --lingerms=5000,7000 --rippct=0.1 --maxdpc=1 -d 0.4 --async --glitchms 500,500 --bold=1 --speed=10 --glitchpct=0.05
+neo --color=green3 --charset=hex --shortpct=0 --shadingmode=1 --lingerms=5000,7000 --rippct=10 --maxdpc=1 -d 0.4 --async --glitchms 500,500 --bold=1 --speed=10 --glitchpct=5
